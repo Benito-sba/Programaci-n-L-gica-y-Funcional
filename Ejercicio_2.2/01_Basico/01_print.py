72bf5d4f989f7fb141cabbf325378879dca9ebf2
Benito-sba <nitoacevedo46@gmail.com>
2026-03-18 20:33:21 -0500

    Initial commit - entrega de ejercicios

---
+++ Ejercicio_2.2/01_Basico/01_print.py
@@ -0,0 +1,29 @@
+from os import system 
+
+
+if system("clear") !=0: system("cls")
+
+
+print("hola, benito!")
+
+print('esto funciona como comiallas')
+
+
+print("hola, benito!", "python", "es", "genial")
+
+
+print("python", "es", "brutal", sep= "-")
+
+
+print("esto es imprimir", end = "\n")
+
+print("en una linea")
+
+print(34)
+
+print('Esto es una "pulgada"  dentro de un string con comillas simples')
+
+
+print("esto \"pulgada\" dentro ")
+
+print('''Esto es una "pulgada"  dentro de un string con comillas simples''')
